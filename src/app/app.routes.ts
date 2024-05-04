05f767222446e9d05d67b18575fd3d838051f0c5
Muhamad Nuime <moesnua@gmail.com>
2024-05-04 23:06:42 +0300

    fix some issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,8 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
-  },
+  // {
+  //   path: 'login',
+  //   loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  // },
 ];
