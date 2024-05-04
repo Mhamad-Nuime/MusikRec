b0eb7f35f64c87c77b3fb53f16cc6f2d9db1c6e6
Muhamad Nuime <moesnua@gmail.com>
2024-05-04 11:13:35 +0300

    configration files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MusicRec',
+  webDir: 'www'
+};
+
+export default config;
