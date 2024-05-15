1b1d5ebf01bb2dad201ef271001859c638d3cc51
Muhamad Nuime <moesnua@gmail.com>
2024-05-15 21:51:57 +0300

    design and implement the media player

--- src/app/content/content.page.ts
+++ src/app/content/content.page.ts
@@ -1,6 +1,18 @@
-import { Component, EnvironmentInjector, ViewChild, inject} from '@angular/core';
+import {
+  Component,
+  ViewChild,
+} from '@angular/core';
 import { addIcons } from 'ionicons';
-import { home, homeOutline, search, searchOutline, library, libraryOutline, playCircle, pauseCircle } from 'ionicons/icons';
+import {
+  home,
+  homeOutline,
+  search,
+  searchOutline,
+  library,
+  libraryOutline,
+  playCircle,
+  pauseCircle,
+} from 'ionicons/icons';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
@@ -8,39 +20,70 @@ import {
   IonIcon,
   IonTabBar,
   IonLabel,
-  IonTabButton, IonRow, IonCol, IonButton } from '@ionic/angular/standalone';
+  IonTabButton,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonItem,
+  IonThumbnail, IonText, IonProgressBar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.page.html',
   styleUrls: ['./content.page.scss'],
   standalone: true,
-  imports: [IonButton, IonCol, IonRow, 
+  imports: [IonProgressBar, IonText, 
+    IonItem,
+    IonButton,
+    IonCol,
+    IonRow,
     IonTabButton,
     IonLabel,
     IonTabBar,
     IonIcon,
     IonTabs,
+    IonThumbnail,
     CommonModule,
     FormsModule,
   ],
 })
 export class ContentPage {
-  public environmentInjector = inject(EnvironmentInjector);
-  
   @ViewChild('tabs') tabs!: IonTabs;
-  selected : string= '';
-  isPaused : boolean = false;
-  progress : number = 42;
+  selected: string = '';
+  isPaused: boolean = false;
+  currentTrackDuration: number = 42;
 
   constructor() {
-    addIcons({ home, homeOutline, search, searchOutline, library, libraryOutline, playCircle, pauseCircle });
+    addIcons({
+      home,
+      homeOutline,
+      search,
+      searchOutline,
+      library,
+      libraryOutline,
+      playCircle,
+      pauseCircle,
+    });
   }
-  
+
   setSelectedTabName() {
     this.selected = this.tabs.getSelected() as string;
   }
-  pauseOrPlay() : void {
-    this.isPaused = !this.isPaused ;
+  pauseOrPlay(): void {
+    this.isPaused = !this.isPaused;
+  }
+  whenSmallMediaPlayerButtonIsClicked() : void {
+    this.pauseOrPlay();
+  }
+  showMediaPlayer() : void {
+    
+  }
+  seekTo(event: any) {
+    const progressBar = event.target;
+    const clickPosition = event.clientX - progressBar.getBoundingClientRect().left;
+    const totalWidth = progressBar.offsetWidth;
+    const percentClicked = (clickPosition / totalWidth) * 100;
+  
+    const desiredTime = (percentClicked / 100) * this.currentTrackDuration;
   }
 }
