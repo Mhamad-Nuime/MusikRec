e127bae91f85a21d3887ad5c996879b464fbe4f5
Muhamad Nuime <moesnua@gmail.com>
2024-06-03 17:14:09 +0300

    refactor: using the password toggle ionic component and use signals to enable and disable the submit button

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -9,6 +9,8 @@ import { environment } from './environments/environment';
 
 import { register } from 'swiper/element/bundle';
 import { HttpClientModule } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 register();
 
 if (environment.production) {
@@ -21,5 +23,7 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-  ],
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 });
